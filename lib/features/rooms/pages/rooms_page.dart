39c4c550ccb7c3a48596128dd3086de98ffff424
Jaime Daza <jaimedavid250701@outlook.com>
2023-11-26 16:20:13 -0500

    feat(rooms): Create main rooms page

--- lib/features/rooms/pages/rooms_page.dart
+++ lib/features/rooms/pages/rooms_page.dart
@@ -11,8 +11,39 @@ class _RoomsPageState extends State<RoomsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Rooms'),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Column(
+                    children: [
+                      const Icon(Icons.hotel),
+                      Text("E${index + 1}"),
+                    ],
+                  ),
+                  title: Text('Habitación ${index + 1}'),
+                  subtitle: Text('Descripción de la habitación ${index + 1}'),
+                  trailing: const Text("S/. 100.00"),
+                ),
+                ButtonBar(
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Editar'),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Eliminar'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
