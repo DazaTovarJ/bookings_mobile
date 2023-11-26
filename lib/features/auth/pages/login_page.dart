39256603e3204de82d2e8c029906a52ae93d20eb
Jaime Daza <jaimedavid250701@outlook.com>
2023-11-25 20:40:20 -0500

    feat(auth): Create login page

---
+++ lib/features/auth/pages/login_page.dart
@@ -0,0 +1,164 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      colors: [
+                        Colors.purple.shade900,
+                        const Color.fromARGB(255, 149, 70, 199),
+                        const Color.fromARGB(255, 155, 51, 173),
+                      ],
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const SizedBox(height: 80),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            FadeInUp(
+                              duration: const Duration(milliseconds: 1000),
+                              child: const Text(
+                                "Bienvenido al Sistema de Reservas de Habitaciones",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white, fontSize: 32),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            FadeInUp(
+                              duration: const Duration(milliseconds: 1300),
+                              child: const Text(
+                                "Inicie sesión para continuar",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white, fontSize: 18),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Expanded(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(60),
+                              topRight: Radius.circular(60),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(30),
+                            child: Column(
+                              children: <Widget>[
+                                const SizedBox(height: 60),
+                                FadeInUp(
+                                  duration: const Duration(microseconds: 1400),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(10),
+                                      boxShadow: const [
+                                        BoxShadow(
+                                            color: Color.fromARGB(224, 69, 3, 95),
+                                            blurRadius: 20,
+                                            offset: Offset(0, 10))
+                                      ],
+                                    ),
+                                    child: Column(
+                                      children: <Widget>[
+                                        Container(
+                                          padding: const EdgeInsets.all(10),
+                                          decoration: BoxDecoration(
+                                            border: Border(
+                                              bottom:
+                                              BorderSide(color: Colors.grey.shade200),
+                                            ),
+                                          ),
+                                          child: const TextField(
+                                            decoration: InputDecoration(
+                                              hintText: "Ingrese Usuario",
+                                              hintStyle: TextStyle(color: Colors.grey),
+                                              border: InputBorder.none,
+                                            ),
+                                          ),
+                                        ),
+                                        Container(
+                                          padding: const EdgeInsets.all(10),
+                                          decoration: BoxDecoration(
+                                            border: Border(
+                                              bottom:
+                                              BorderSide(color: Colors.grey.shade200),
+                                            ),
+                                          ),
+                                          child: const TextField(
+                                            obscureText: true,
+                                            decoration: InputDecoration(
+                                              hintText: "Ingrese Contraseña",
+                                              hintStyle: TextStyle(color: Colors.grey),
+                                              border: InputBorder.none,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 40),
+                                FadeInUp(
+                                  duration: const Duration(milliseconds: 1600),
+                                  child: MaterialButton(
+                                    onPressed: () {},
+                                    height: 50,
+                                    color: Colors.purple[900],
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                    child: const Center(
+                                      child: Text(
+                                        "Login",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
+    );
+  }
+}
