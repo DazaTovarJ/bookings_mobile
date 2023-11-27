9463d5db667a24c080fa078cf8a6e3c9599b2804
Jaime Daza <jaimedavid250701@outlook.com>
2023-11-27 12:50:00 -0500

    refactor(ui): Set main component ui colors

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,47 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var colorSchemeLight = ColorScheme.fromSeed(
+      seedColor: const Color(0xFF262254),
+      error: const Color(0xFFBF002E),
+      errorContainer: const Color(0xFFFFDAD9),
+      onErrorContainer: const Color(0xFF400009),
+    );
     var appTheme = ThemeData(
-      colorScheme: ColorScheme.fromSeed(
-        seedColor: const Color(0xFF262254),
-        error: const Color(0xFFBF002E),
-        errorContainer: const Color(0xFFFFDAD9),
-        onErrorContainer: const Color(0xFF400009),
+      colorScheme: colorSchemeLight,
+      scaffoldBackgroundColor: colorSchemeLight.surface,
+      appBarTheme: AppBarTheme(
+        backgroundColor: ElevationOverlay.applySurfaceTint(
+          colorSchemeLight.surface,
+          colorSchemeLight.surfaceTint,
+          3,
+        ),
+        foregroundColor: colorSchemeLight.onSurface,
+        elevation: 0,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+            (Set<MaterialState> states) {
+              if (states.contains(MaterialState.disabled)) {
+                return colorSchemeLight.onSurface.withOpacity(0.12);
+              }
+              return ElevationOverlay.applySurfaceTint(
+                colorSchemeLight.surface,
+                colorSchemeLight.surfaceTint,
+                1,
+              );
+            },
+          ),
+          foregroundColor: MaterialStateProperty.resolveWith<Color?>(
+            (Set<MaterialState> states) {
+              if (states.contains(MaterialState.disabled)) {
+                return colorSchemeLight.onSurface.withOpacity(0.38);
+              }
+              return colorSchemeLight.primary;
+            },
+          ),
+        ),
       ),
       useMaterial3: true,
     );
